38059950186b26efcfc981a96df45158a9ae4d1d
Евгений <trewor.92@gmail.com>
2019-04-07 22:56:00 +0400

    task-4 initial commit

---
+++ ConsoleEnumerable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task target = new Task();
+            
+              
+
+            Console.ReadLine();
+
+        }
+
+    
+    }
+}
